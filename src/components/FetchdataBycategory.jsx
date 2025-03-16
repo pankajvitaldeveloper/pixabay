e6c88e14114a9b06ed0223c90b7b4e318906abf6
pankajvitaldeveloper <pankajvitaldeveloper0@example.com>
2025-03-16 07:46:50 +0530

    upload again

--- src/components/FetchdataBycategory.jsx
+++ src/components/FetchdataBycategory.jsx
@@ -8,15 +8,15 @@ const FetchdataBycategory = () => {
     <div className='conntainer'>
         <div className="row">
         <div className="col-md-12">
-              <div className="button-wrap d-flex justify-content-between">
-              <button type="button" onClick={()=>fetchDataByCategory('nature')} className="btn btn-primary">Nature</button>
-              <button type="button" onClick={()=>fetchDataByCategory('science')} className="btn btn-secondary">Science</button>
-              <button type="button" onClick={()=>fetchDataByCategory('health')} className="btn btn-success">Health</button>
-              <button type="button" onClick={()=>fetchDataByCategory('computer')} className="btn btn-danger">Computer</button>
-              <button type="button" onClick={()=>fetchDataByCategory('food')} className="btn btn-warning">Food</button>
-              <button type="button" onClick={()=>fetchDataByCategory('sports')} className="btn btn-info">Sports</button>
-              <button type="button" onClick={()=>fetchDataByCategory('travel')} className="btn btn-light">Travel</button>
-              <button type="button" onClick={()=>fetchDataByCategory('music')} className="btn btn-dark">Music</button>
+              <div className="button-wrap d-flex justify-content-around flex-wrap">
+              <button type="button" onClick={()=>fetchDataByCategory('nature')} className="btn mt-3 btn-primary">Nature</button>
+              <button type="button" onClick={()=>fetchDataByCategory('science')} className="btn mt-3 btn-secondary">Science</button>
+              <button type="button" onClick={()=>fetchDataByCategory('health')} className="btn mt-3 btn-success">Health</button>
+              <button type="button" onClick={()=>fetchDataByCategory('computer')} className="btn mt-3 btn-danger">Computer</button>
+              <button type="button" onClick={()=>fetchDataByCategory('food')} className="btn mt-3 btn-warning">Food</button>
+              <button type="button" onClick={()=>fetchDataByCategory('sports')} className="btn mt-3 btn-info">Sports</button>
+              <button type="button" onClick={()=>fetchDataByCategory('travel')} className="btn mt-3 btn-light">Travel</button>
+              <button type="button" onClick={()=>fetchDataByCategory('music')} className="btn mt-3 btn-dark">Music</button>
               </div>
             </div>
             
